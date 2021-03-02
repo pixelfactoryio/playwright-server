2012a72082c8d1a353efab19485869db9d534f8e
Amine Benseddik <amine.benseddik@gmail.com>
2021-03-02 16:18:11 +0100

    Add firefox webkit (#1)
    * expose server.port number
    
    * feat: add support for firefox and webkit
    
    Co-authored-by: Amine Benseddik <amine@pixelfactory.io>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import ecsFormat from '@elastic/ecs-winston-format';
-import { chromium } from 'playwright';
+import playwright from 'playwright';
 import { createLogger, transports } from 'winston';
 
 import ProxyServer from './Server';
@@ -10,9 +10,32 @@ const logger = createLogger({
   transports: [new transports.Console()],
 });
 
+let browserType = process.env.BROWSER_TYPE || 'chromium';
+
+const startBrowserServer = async (type: string): Promise<playwright.BrowserServer> => {
+  switch (type) {
+    case 'chromium': {
+      return playwright['chromium'].launchServer();
+    }
+    case 'firefox': {
+      return playwright['firefox'].launchServer();
+    }
+    case 'webkit': {
+      return playwright['webkit'].launchServer();
+    }
+    default: {
+      logger.error(`unkown browserType ${browserType}, defaulting to chromium`);
+      browserType = 'chromium';
+      break;
+    }
+  }
+  return playwright['chromium'].launchServer();
+};
+
 (async (): Promise<void> => {
-  const browserServer = await chromium.launchServer();
+  const browserServer = await startBrowserServer(browserType);
   const wsEndpoint = browserServer.wsEndpoint();
+  logger.info(`starting ${browserType} browser server ${wsEndpoint}`);
 
   const server = new ProxyServer({
     logger,
@@ -20,5 +43,6 @@ const logger = createLogger({
     port: Number(process.env.PORT) || 3000,
   });
 
+  logger.info(`starting websocket proxy on port ${server.port}`);
   server.listen();
 })();
